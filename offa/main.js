177e27dfc8f6618026dd0b3e67066e76df33f0c5
youssefmahmoud2000 <116124782+youssefmahmoud2000@users.noreply.github.com>
2022-12-06 16:03:34 +0200

    Add files via upload

---
+++ offa/main.js
@@ -0,0 +1,46 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+const toggle = document.getElementById('night');
+const body = document.querySelector('body');
+toggle.addEventListener('click', function(){
+this.classList.toggle('fa-moon');
+body.classList.toggle('dark');
+
+
+})
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_fvyodbj";
+    const templateID = "template_ffk0i4b";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+
